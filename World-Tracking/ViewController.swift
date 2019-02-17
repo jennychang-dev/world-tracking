efa89446e05864968cdb28a0aedcc5b5f9da220c
Jenny Chang <jennychang@Jennys-MacBook-Pro-2.local>
2019-02-17 12:23:07 -0800

    oh myyyyy so many blue boxes, v cool

--- World-Tracking/ViewController.swift
+++ World-Tracking/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
 ////////////////////////////////////////////////////////////////////
         
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.autoenablesDefaultLighting = true // now allows orange light to be able to be reflected (omni is the default - light that is spread across the entire scene)
         
         self.sceneView.session.run(configuation)
     }
@@ -39,7 +40,8 @@ class ViewController: UIViewController {
 // firstMaterial - appearance, diffuse - the colour that's spread across the entire surface
 ////////////////////////////////////////////////////////////////////
         
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03) // to make a sphere, make chamferRadius 1/2 of other measurements
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange // specular that is light that is reflected off a surface, but we need to give the scene view a source of light for it to be able to reflect it
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         
 ////////////////////////////////////////////////////////////////////
@@ -47,23 +49,26 @@ class ViewController: UIViewController {
 // how far something away is relative to our starting position (root node)
 ////////////////////////////////////////////////////////////////////
         
-        node.position = SCNVector3(0,0,0)
+        let x = randomNumbers(firstNum: -0.3, secondNum: 0.3) // this will return a float value between 0.3 to the left of the world origin and 0.3 to the right
+        let y = randomNumbers(firstNum: -0.3, secondNum: 0.3) // this will return a float value between 0.3 below the world origin and 0.3 above
+        let z = randomNumbers(firstNum: -0.3, secondNum: 0.3) // this will return a float value between 0.3 behind and 0.3 in front of the world origin
+        
+        node.position = SCNVector3(x,y,z) // now we have a random x,y,z vector for every shape we add
         self.sceneView.scene.rootNode.addChildNode(node)
 
 ////////////////////////////////////////////////////////////////////
 // now if I were to 'add' my box, it'll get placed right where the starting position is (0,0,0) - (x,y,z)
 ////////////////////////////////////////////////////////////////////
-        
 
     }
     
     @IBAction func reset(_ sender: Any) {
+        
 ////////////////////////////////////////////////////////////////////
 // every time I click add, I'm adding another blue box in the same position, we just can't see it. How can we change the box position? Reset tracking and change starting position!!
 ////////////////////////////////////////////////////////////////////
         
         self.restartSession()
-        
     }
     
     func restartSession() {
@@ -72,6 +77,17 @@ class ViewController: UIViewController {
             node.removeFromParentNode() // remove box from parent node, remove from scene view
         }
         self.sceneView.session.run(configuation, options: [.resetTracking, .removeExistingAnchors]) // forget about old starting position and make a new one based on where we are at the moment and remove existing anchors (position and orientation info)
+    }
+    
+    
+////////////////////////////////////////////////////////////////////
+// creating multiple shapes
+// function takes a min float value and a max float value and generates a float value inbetween
+// this will be used to place a box in random directions instead of just 1
+////////////////////////////////////////////////////////////////////
+    
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
 }
 
